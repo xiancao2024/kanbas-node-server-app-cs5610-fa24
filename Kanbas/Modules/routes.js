e20b035f0158af313a5a3720be836f5741f29814
xiancao2024 <cao.xian@northeastern.edu>
2024-12-03 10:15:25 -0800

    test module

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,9 +1,9 @@
 import * as modulesDao from "./dao.js";
 
 export default function ModuleRoutes(app) {
-  app.delete("/api/modules/:moduleId", (req, res) => {
+  app.delete("/api/modules/:moduleId", async(req, res) => {
     const { moduleId } = req.params;
-    const status = modulesDao.deleteModule(moduleId);
+    const status = await modulesDao.deleteModule(moduleId);
     res.send(status);
   });
 
